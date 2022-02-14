99ee8397d64229cfebccdaccf8a40f26558c4a3d
Xavier Savoie <xaviersavoie@gmail.com>
2022-02-14 14:09:56 -0500

    02 - objects

--- 02/app.js
+++ 02/app.js
@@ -1,12 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    return result;
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: "Xavier",
+    age: 30
+};
+console.log(person);
