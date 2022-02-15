6848556ca73e05ebda1991134b8f702605124344
Xavier Savoie <xaviersavoie@gmail.com>
2022-02-15 14:51:00 -0500

    02 - types function

--- 02/app.js
+++ 02/app.js
@@ -1,23 +1,10 @@
-function combine(input1, input2, resultConversion // give explicit value to type
-) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultConversion === 'as-number') {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
--- 02/app.ts
+++ 02/app.ts
@@ -1,29 +1,16 @@
-type Combinable = number | string;
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: 'as-number' | 'as-text' // give explicit value to type
-) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  if (resultConversion === 'as-number') {
-    return +result;
-  } else {
-    return result.toString();
-  }
-  return result;
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges)
+printResult(add(5, 12));
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+// let combineValues: Function;
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
