da2754288747865f11653680475f8bfac1b0a33a
Xavier Savoie <xaviersavoie@gmail.com>
2022-02-15 13:34:01 -0500

    02 - types array

--- 02/app.js
+++ 02/app.js
@@ -1,5 +1,18 @@
+// explicitely define object and keys
+// const person: {
+//   name: string
+//   age: number
+// } = {
 var person = {
     name: "Xavier",
-    age: 30
+    age: 30,
+    hobbies: ['Cooking', 'Reading']
 };
+// let favoriteActivities: string[];
+var favoriteActivities;
+favoriteActivities = ['Cooking', 1];
 console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toString());
+}
--- 02/app.ts
+++ 02/app.ts
@@ -1,6 +1,21 @@
-const person: object = {
+// explicitely define object and keys
+// const person: {
+//   name: string
+//   age: number
+// } = {
+  const person = {
   name: "Xavier",
-  age: 30
+  age: 30,
+  hobbies: ['Cooking', 'Reading']
 };
 
+// let favoriteActivities: string[];
+let favoriteActivities: any[];
+favoriteActivities = ['Cooking', 1]
+
 console.log(person);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toString());
+  // console.log(hobby.map()) // error because map is not available on strings
+}
