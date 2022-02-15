0ebae06253eaf91395b85e9469deeefe48aad7b0
Xavier Savoie <xaviersavoie@gmail.com>
2022-02-15 15:07:23 -0500

    02 - types unknown and never

--- 02/app.ts
+++ 02/app.ts
@@ -1,25 +1,14 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
 
-function printResult(num: number): void {
-  console.log('Result: ' + num);
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result); 
-}
-
-printResult(add(5, 12));
-
-// let combineValues: Function;
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-})
+generateError('An error occurred!', 500);
