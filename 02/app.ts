f7fb57249613c05a17f5c3117f9efc0a4053a59e
Xavier Savoie <xaviersavoie@gmail.com>
2022-02-15 14:57:32 -0500

    02 - types function callbacks

--- 02/app.ts
+++ 02/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result); 
+}
+
 printResult(add(5, 12));
 
 // let combineValues: Function;
@@ -14,3 +19,7 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+})
