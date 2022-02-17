54e60bf8b79d8297655b8ac16ddf732f2edd7110
Xavier Savoie <xaviersavoie@gmail.com>
2022-02-17 16:23:32 -0500

    03 tsconfid outDir + rootDir

--- typescript/03/app.ts
+++ typescript/03/app.ts
@@ -1,7 +1,9 @@
-"use strict";
 // tsc --init --> creates tsconfig.json file
 // can now use tsc command to compile all ts files in dir OR tsc -w to watch all
-const button = document.querySelector('button');
+
+const button = document.querySelector('button')!;
 button.addEventListener('click', () => {
-    console.log('Click');
+  console.log('Click');
 });
+
+// test
--- typescript/app.ts
+++
@@ -1,7 +0,0 @@
-// tsc --init --> creates tsconfig.json file
-// can now use tsc command to compile all ts files in dir OR tsc -w to watch all
-
-const button = document.querySelector('button')!;
-button.addEventListener('click', () => {
-  console.log('Click');
-});
