116cdd057ef9177f2985050617cd33061e02bed4
Xavier Savoie <xaviersavoie@gmail.com>
2022-02-21 13:49:33 -0500

    05 - classes ts

--- typescript/05/app.ts
+++ typescript/05/app.ts
@@ -4,8 +4,15 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
+  }
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting);
+accounting.describe();
+
+const accountingCopy = { describe: accounting.describe };
+accountingCopy.describe();
--- typescript/dist/05/app.js
+++ typescript/dist/05/app.js
@@ -3,6 +3,11 @@ class Department {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
 }
 const accounting = new Department('Accounting');
-console.log(accounting);
+accounting.describe();
+const accountingCopy = { describe: accounting.describe };
+accountingCopy.describe();
